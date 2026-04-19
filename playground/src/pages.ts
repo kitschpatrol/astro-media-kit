eb01607c1cd19d5237beabf8933def77249b68db
Eric Mika <eric@ericmika.com>
2026-04-19 01:40:30 -0400

    Picture srcset fixes.

--- playground/src/pages.ts
+++ playground/src/pages.ts
@@ -1,6 +1,7 @@
 export const pages = [
 	{ href: '/image-component', title: 'Image' },
 	{ href: '/picture-component', title: 'Picture' },
+	{ href: '/srcset', title: 'Srcset' },
 	{ href: '/video-component', title: 'Video' },
 	{ href: '/audio-component', title: 'Audio' },
 	{ href: '/zoom', title: 'Zoom' },
